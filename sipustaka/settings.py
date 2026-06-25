c919eb883e692fe5abb7e82a212a9e69bf396230
khosyi abqory prayata <khosyiprayata@gmail.com>
2026-06-25 08:13:24 +0700

    Update fitur perpustakaan

--- sipustaka/settings.py
+++ sipustaka/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-sipustaka-dev-key"
 DEBUG = True
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["https://khosyi.pythonanywhere.com/"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -63,3 +63,9 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
